e3ffa186a25ab63d73288b3df342e00e466e79b4
Raj Keshari <Rajkeshari746@gmail.com>
2023-08-19 13:47:40 +0530

    Removing Cors error

--- index.js
+++ index.js
@@ -27,23 +27,35 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ['http://localhost:3000', 'https://64dfaf073dd4c2053a2c473c--velvety-croissant-4dd6f9.netlify.app'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://majestic-kitsune-23701c.netlify.app",
-      "https://e-creativity.herokuapp.com/",
-      "http://127.0.0.1:5500/index.html",
-      "http://127.0.0.1:5500/index%20(1).html",
-      "http://127.0.0.1:5500",
-      "https://stellar-donut-7119ad.netlify.app/",
-      "https://64dfaf073dd4c2053a2c473c--velvety-croissant-4dd6f9.netlify.app/",
-    ],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3000",
+//       "https://majestic-kitsune-23701c.netlify.app",
+//       "https://e-creativity.herokuapp.com/",
+//       "http://127.0.0.1:5500/index.html",
+//       "http://127.0.0.1:5500/index%20(1).html",
+//       "http://127.0.0.1:5500",
+//       "https://stellar-donut-7119ad.netlify.app/",
+//       "https://64dfaf073dd4c2053a2c473c--velvety-croissant-4dd6f9.netlify.app/",
+//     ],
+//     credentials: true,
+//   })
+// );
 
 // set up routes
 
